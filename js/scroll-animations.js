1498ff25e3603d0409f2f81d5d378d8107ce68a2
ewhillier <eve@whillier.com>
2017-02-27 12:34:09 -0700

    analysis animations
    still need to figure out the colored states animation

---
+++ js/scroll-animations.js
@@ -0,0 +1,123 @@
+$(window).scroll(function() {
+		$('#armTickScroll').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+700) {
+				$(this).addClass("armTickAnimate");
+			}
+		});
+	});
+	
+$(window).scroll(function() {
+	$('#armRashScroll').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+1200) {
+			$(this).addClass("armRashAnimate");
+		}
+	});
+});
+
+$(window).scroll(function() {
+	$('#armSpirocheteScrollOne').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+1200) {
+			$(this).addClass("armSpirocheteAnimate");
+		}
+	});
+});
+
+$(window).scroll(function() {
+	$('#armSpirocheteScrollTwo').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+1200) {
+			$(this).addClass("armSpirocheteAnimate");
+		}
+	});
+});
+
+$(window).scroll(function() {
+	$('#armSpirocheteScrollThree').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+1200) {
+			$(this).addClass("armSpirocheteAnimate");
+		}
+	});
+});
+
+
+// ANALYSIS ANIMATIONS
+
+$(window).scroll(function() {
+	$('#increasingDescription').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+600) {
+			$(this).addClass("revealIncreasingDescription");
+		}
+	});
+	
+	$('#increasingFilled').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+300) {
+			$(this).addClass("graphIncreasingFillAnimation");
+		}
+	});
+	
+	$('#descreasingDescription').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+500) {
+			$(this).addClass("revealDecreasingDescription");
+		}
+	});
+	
+	$('#oppositeDescription').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+600) {
+			$(this).addClass("revealOppositeDescription");
+		}
+	});
+	
+	$('#oppositeFilled').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("graphOppositeFillAnimation");
+		}
+	});
+	
+	$('#percentageDescription').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+700) {
+			$(this).addClass("revealPercentageDescription");
+		}
+	});
+	
+	$('#percentageFilled').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+500) {
+			$(this).addClass("graphPercentageFillAnimation");
+		}
+	});
+	
+});
